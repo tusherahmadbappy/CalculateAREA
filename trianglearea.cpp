a21078cdf670c8d1a862ca2569a0236ae7655f8c
Md. Tusher Ahmad Bappy <106505665+tusherahmadbappy@users.noreply.github.com>
2022-11-06 15:18:35 +0600

    Calculate triangle area

---
+++ trianglearea.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double hight,width,area=0;
+    cout<<"Enter hight & width: ";
+    cin>>hight>>width;
+    
+    area = 0.5*width*hight;
+    
+    cout<<"Triangle area is: "<<area<<endl;
+    
+    return 0;
+}
